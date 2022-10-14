6aa9b0705d9826295dc69c4afac318eda35dffe1
cristian guerrero <cristian1guerrero@hotmail.com>
2022-10-14 09:11:16 -0300

    restas definidas con express

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const express = require('express')
 const PORT = process.env.PORT || 8080
 const app = express();
 const {herramientaFechas}= require('./utils/herramientaFechas')
+const noEncuntrado = 404
+
+
+//! rutas 
+//require = req --- response= res
+app.get("/",(req, res)=>{
+    res.send(`biemvenido a la pagina, son las ${herramientaFechas.hora} del dia ${herramientaFechas.fecha}`)
+})
+app.get("/carrito",(req,res)=>{
+    res.send('<h1>carrito</h1>')
+})
+app.get("/productos",(req,res)=>{
+    res.send('<h1>productos</h1>')
+})
+app.get("/secret",(req,res)=>{
+    res.send('<h1>te dejo un genial y fabuloso 404.    Denada bebe</h1>')
+})
+app.get("*",(req,res)=>{
+    res.status(noEncuntrado).send(`${noEncuntrado} pagina no existe`)
+})
 
 
 
@@ -12,7 +32,6 @@ const connectedServer = app.listen(PORT,()=>{
     console.log(`servidor activo en ${PORT}`)
     console.log(`se conecto un usuario al puerto ${PORT} a las ${herramientaFechas.hora} del dia ${herramientaFechas.fecha}`)
 })
-
 
 connectedServer.on('error',(error)=>{
     console.log(error.message)
